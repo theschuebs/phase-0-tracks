21ad04f61aa8f73729e262f9a19c59216a093a10
erika.schuebel <erika.schuebel@gmail.com>
2016-03-25 11:22:53 -0500

    Challenge 4.3 completed with nil

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,9 @@
 #build a ruby program that asks the user for following data and stores them in variables:
-#hamster's name (string)
-#volume level from 1 to 10 (integer)
-#fur color (string)
-#is the hamster a good candidate for adoption? (boolean)
-#estimated age (if this is blank, set variable for estimated age to nil)
+	#hamster's name (string)
+	#volume level from 1 to 10 (integer)
+	#fur color (string)
+	#is the hamster a good candidate for adoption? (boolean)
+	#estimated age (if this is blank, set variable for estimated age to nil)
 
 puts "What is your hamster's name?"
 hamster_name = gets.chomp
@@ -24,26 +24,15 @@ estimated_age = gets.chomp.to_i
 	puts "#{hamster_name}'s volume level is a #{volume_level}."
 	puts "#{hamster_name} has #{fur_color} fur."
 
-	if adoption_candidate == "Y"
-			puts "Yes, #{hamster_name} is a great candidate for adoption!"
-	else
-			puts "No, #{hamster_name} is not a good candidate for adoption."
-	end
+		if adoption_candidate == "Y"
+				puts "Yes, #{hamster_name} is a great candidate for adoption!"
+		else
+				puts "No, #{hamster_name} is not a good candidate for adoption."
+		end
 
 
-	# if estimated_age != nil
-	# 	puts "#{hamster_name}'s age is #{estimated_age}."
-	# end
-	if estimated_age = integer
-		puts "My hamster's age is #{estimated_age}"
-	end
-	 unless estimated_age == nil
-		puts "It's ok, we will find out."
-	end
-
-
-	# else
-	# 	puts "My hamster's age is #{estimated_age}"
-	# end
-
-
+		if estimated_age > 0
+			puts "#{hamster_name}'s age is #{estimated_age}"
+		else estimated_age == nil
+			puts "It's ok, we will find out #{hamster_name}'s age."
+		end
