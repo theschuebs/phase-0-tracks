f6103ac50b72d26a0d4bff435cc3755489a0d2df
erika.schuebel <erika.schuebel@gmail.com>
2016-03-28 11:48:40 -0500

    Fix detection logic bugs and add employees processed question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees will be processed?"
+	employees_processed = gets.chomp
+
+
+#User Input and data gathering
 
 puts "Hello! What is your name?"
 	name = gets.chomp
@@ -7,55 +12,31 @@ puts "In what year were you born?"
 
 puts "#{name}, how old are you?"
 	age = gets.chomp.to_i
-	current_year = Time.now.year
-
-	if age == (current_year - birth_year)
-		input = true
-	else
-		input = false
-	end
 
 puts "Our cafeteria serves garlic bread. Should we order some for you? (y/n)"
 	garlic_bread = gets.chomp
 
-	if garlic_bread == "y"
-	  order_some = true
-	else
-	  order_some = false
-	end
-
 puts "Would you like to enroll in our health insurance? (y/n)"
 	health_insurance = gets.chomp
 
-	if health_insurance == "y"
-	  enroll = true
-	else
-	  enroll = false
-	end
-
+#Detection Logic
+	
 # if the employee got their age right, and is willing to eat garlic bread or sign up for insurnace
-	if age && (garlic_bread || health_insurance)
+	if age == (2016 - birth_year) && (garlic_bread == "y" || health_insurance == "y")
 		puts "Probably not a vampire."
-
 # # if age is wrong and hates garlic bread or waves insurance
-	elsif !age && (garlic_bread || health_insurance)
+	elsif age != (2016 - birth_year) && (garlic_bread == "n" || health_insurance == "n")
 		puts "Probably a vampire."
-
 # # if age is wrong, hates garlic bread and doesn't want insurance
-	elsif !age && (garlic_bread && health_insurance)
+	elsif age != (2016 - birth_year) && (garlic_bread == "n" && health_insurance == "n")
 		puts "Almost certainly a vampire"
-
-	# If name is "Drake Cula" or "Tu Fang"
-	elsif name == "Drake Cula" || name == "Tu Fang"
-		puts "Definitely a vampire"
 	else
 		puts "Results inconclusive"
 	end
 
+	# If name is "Drake Cula" or "Tu Fang"
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire"
+	end
 
-# Notes
-	 # 
-# Stick to a loop based off the string "y"
-# garlic_bread.upcase() == 'Y'
-# health_insurance.upcase() == 'Y'
-
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
