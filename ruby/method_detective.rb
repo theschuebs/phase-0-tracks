127573c841e887000b4f9952456bbbd43f6e556b
erika.schuebel <erika.schuebel@gmail.com>
2016-03-29 18:35:46 -0500

    Fix merge conflict

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,7 +4,6 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-<<<<<<< HEAD
 "iNvEsTiGaTiOn".swapcase
 => "InVeStIgAtIoN"
 
@@ -34,44 +33,8 @@
 
 "z".ord
 => 122 
-=======
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
-
-# "zom".<???>
-# => “zoom”
-
-# "enhance".<???>
-# => "    enhance    "
-
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
-
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
-# => "the usual suspects"
-
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
-
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
-
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
-
-# "z".<???>
-# => 122 
->>>>>>> upstream/master
 # (What is the significance of the number 122 in relation to the character z?)
 	## The relation is that 122 is the "z" ascii representation
 
-<<<<<<< HEAD
 "How many times does the letter ‘a’ appear in this string?".count('a')
 => 4
-=======
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
->>>>>>> upstream/master
