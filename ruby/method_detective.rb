4f0d9167acb56d4a0c91308d9114c1f5f340ec8a
erika.schuebel <erika.schuebel@gmail.com>
2016-03-29 17:44:47 -0500

    Use string methods to solve problems

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> "InVeStIgAtIoN"
 
-# “zom”.<???>
-# => “zoom”
+"zom".insert(1, "o")
+=> "zoom"
 
-# “enhance”.<???>
-#=> “    enhance    ”
+"enhance".center(15)
+=> "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# “the usual”.<???>
-#=> “the usual suspects”
+"the usual".concat(" suspects")
+=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+" suspects".prepend("the usual")
+=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+"The mystery of the missing first letter".slice(1..-1)
+=> "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+"Elementary  ,    my   dear      Watson!".replace("Elementary, my dear Watson!")
+=> "Elementary, my dear Watson!"
 
-#“z”.<???>
-# => 122 
+"z".ord
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
+	## The relation is that 122 is the "z" ascii representation
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
-#=> 4
+"How many times does the letter ‘a’ appear in this string?".count('a')
+=> 4
