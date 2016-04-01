9cf2f05ed7e33b82a6f13226ec44f494e7e69d6a
erika.schuebel <erika.schuebel@gmail.com>
2016-04-01 11:24:56 -0500

    Use each call on array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -12,3 +12,33 @@ end
 
 
 block_runner { |first, second| puts "#{first} #{second}" }
+
+
+
+pizza_toppings = ["sausage", "pepperoni", "cheese", "pineapple"]
+
+phone_book = {"Bob" => 1702292, "Sally" => 5555555,  "Joe" => 1234567}
+
+# EACH #
+
+# before array each call
+p pizza_toppings
+
+# array each call
+pizza_toppings.each do |topping|
+  puts "I like this topping: #{topping}"
+end
+
+# after array each call
+p pizza_toppings
+
+# before hash each call
+p phone_book
+
+# hash each call
+phone_book.each do |name, number|
+  puts "You can reach #{name} at #{number}"
+end
+
+# after hash each call
+p phone_book
