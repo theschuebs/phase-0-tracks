88a8c6352a9949b35c32389e0211130bd7618286
erika.schuebel <erika.schuebel@gmail.com>
2016-04-06 11:37:32 -0500

    Initial commit

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,34 @@ class Puppy
     toy
   end
 
-end
+  def speak(number)
+      number.times do
+        puts "Woof!"
+      end
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
 
+  # roll_over
+
+  def dog_years(dog_age)
+    human_age = dog_age * 7
+    puts human_age
+  end
+
+  # dog_years(5)
+
+  def play_dead(seconds)
+    puts "The dog plays dead for #{seconds} seconds."
+  end
+
+  # play_dead(30)
+
+  end
+
+puppy = Puppy.new
+
+puppy.fetch("ball")
+puppy.speak(3)
