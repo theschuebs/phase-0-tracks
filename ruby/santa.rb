5251ce31ba2aaab4a9b8573ed847d7037b530572
erika.schuebel <erika.schuebel@gmail.com>
2016-04-07 19:13:46 -0500

    Add shuffle santa generator

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,18 +2,15 @@
 class Santa
 	#refactor the getter methods using attr_reader
 	attr_reader :gender, :ethnicity
-	# attr_accessor :celebrate_birthday :get_mad_at
+	attr_accessor :age, :reindeer_ranking
+
 #Define multiple instance methods including initialize method
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		@age = 0
+		@age = rand(0..140)
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		puts "Initializing Santa instance ..."
-	end
-
-	def santa_characteristics
-		puts "I am a #{@gender} #{@ethnicity} santa!"
 	end
 
 	def speak
@@ -24,7 +21,7 @@ class Santa
 		puts "That was a good #{cookie_type}!" 
 	end
 
-#These getter methods have been refactored using attr_reader
+#Getter methods have been refactored using attr_reader
 	# def gender
 	# 	@gender
 	# end
@@ -33,36 +30,36 @@ class Santa
 	# 	@ethnicity
 	# end
 
-	# def celebrate_birthday(current_age)
-	# 	@celebrate_birthday = current_age +1
-	# end
-
-	def celebrate_birthday=(current_age)
-		@celebrate_birthday = current_age + 1
+# Setter methods
+	def celebrate_birthday=(age)
+		@age = @age + 1
 	end
 
-	# def get_mad_at(reindeer_name)
-	# 	@get_mad_at = reindeer_name
-	# end
+	def get_mad_at(reindeer_name)
+		index = @reindeer_ranking.index(reindeer_name)
+		@reindeer_ranking.delete_at(index)
+		@reindeer_ranking.push(reindeer_name)
+		puts "Updating the reindeer ranks: #{@reindeer_ranking}" 
+	end
 
+end
+
+# #Data bucket
+santas = []
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+gender.length.times do |i|
+  santas << Santa.new(gender[i], ethnicity[i])
 end
 
 #DRIVER CODE
 #Create new class instance
-santa = Santa.new("male", "Caucasion")
+santa = Santa.new(gender, ethnicity)
 
 #Call instance methods on new class instance
-santa.santa_characteristics
 santa.speak
 santa.milk_and_cookies("Thin Mint")
-puts "This santa is a #{santa.ethnicity} #{santa.gender}" 
+puts "This santa is a #{santa.ethnicity.shuffle[0]} #{santa.gender.shuffle[0]}" 
+santa.get_mad_at("Vixen")
 # puts "Santa is turning #{santa.celebrate_birthday(5)}!"
-# puts "Updating the reindeer ranks: #{santa.get_mad_at("Vixen")} has been demoted."
-santa.celebrate_birthday = "Santa has a birthday to celebrate"
-# #Create new class instance
-# santa = Santa.new("female", "Latino")
-
-# #Call instance methods on new class instance
-# santa.santa_characteristics
-# santa.speak
-# santa.milk_and_cookies("Samoa")
+p santa.celebrate_birthday = "Santa is turning #{@age}!"
