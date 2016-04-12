ff07fc199fce2b59afd1c954e208b8205d03d644
erika.schuebel <erika.schuebel@gmail.com>
2016-04-11 23:31:11 -0500

    Add pseudocode

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,5 +1,27 @@
-# #Implement a class
+## Add User Interface
+	#Allow user to create many instances of the tent class
+		#Input: color, brand, guests
+		#Output:
+	#prompt user for each attribute and convert to appropriate data type
+		# Input: 
+		puts "What color would you like your tent to be?"
+		@color = gets.chomp
 
+		puts "Which brand would you like to purchase?"
+		user_brand = gets.chomp
+
+		puts "How many campers are you planning on housing?"
+		user_guests = gets.chomp
+		#Output: Take user data and store in an array
+
+	#store class instances in an array
+		#Input:
+		#Output:
+	#When user is finished, loop through the array and print out confirmation message about what was created.
+		#Input:
+		#Output:
+
+# #Implement a class for Release 0 and 1. 
 class Tent
 	attr_reader :brand, :guests
 	attr_accessor :color
@@ -45,23 +67,26 @@ class Tent
 	# 	@color = new_color
 	# end
 end
+
 #Data bucket
 
-tents = []
-color = ["green", "blue", "red", "brown", "beige", "grey", "orange"]
-brand = ["Coleman TM", "North Face TM", "Wenzel TM"]
-guests = [rand(1..12)]
-color.length.times do |i|
-  tents << Tent.new(color[i], brand[i], guests[i])
-end
+# tents = []
+# color = ["green", "blue", "red", "brown", "beige", "grey", "orange"]
+# brand = ["Coleman TM", "North Face TM", "Wenzel TM"]
+# guests = [rand(1..12)]
+# color.length.times do |i|
+#   tents << Tent.new(color[i], brand[i], guests[i])
+# end
 
+
+#Release 0 and 1 have been stored below:
 # initialize new instance of the tent class
-tent = Tent.new(color, brand, guests)
-puts "This tent is a #{tent.color.shuffle[0]} #{tent.brand.shuffle[0]}" 
-puts "We have 2 #{tent.color} #{tent.brand} left in stock."
-tent.color = "New! Camouflage color"
-tent.set_up_time(20)
-tent.zip
+# tent = Tent.new(color, brand, guests)
+# puts "This tent is a #{tent.color.shuffle[0]} #{tent.brand.shuffle[0]}" 
+# puts "We have 2 #{tent.color} #{tent.brand} left in stock."
+# tent.color = "New! Camouflage color"
+# tent.set_up_time(20)
+# tent.zip
 
 
 
