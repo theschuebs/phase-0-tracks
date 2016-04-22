fc8a177a1996b7887a161b97b5b059dd6254d1d6
erika.schuebel <erika.schuebel@gmail.com>
2016-04-22 13:54:46 -0500

    Complete release 2 Nighthawk

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,8 +1,18 @@
+#At the top of your file, add a method that takes three items as parameters
+def hockey(x, y, z)
+	#returns an array of those items.
+	[x, y, z]
+end
+
+#At the top of your file, add a method that takes an array and an item as parameters,
+def madhouse(array, item)
+	[] << item
+end
+
 #Initialize an empty array and store it in a variable (you can choose the name).
 blackhawks = []
 #Print the variable using p.
 p blackhawks
-
 
 #Add five items to your array. Print the array.
 blackhawks << "Anisimov"
@@ -11,7 +21,6 @@ blackhawks << "Desjardins"
 blackhawks << "Hossa"
 blackhawks << "Kane"
 p blackhawks
-
 
 # Delete the item at index 2. Print the array.
 blackhawks.delete_at(2)
@@ -40,3 +49,13 @@ player_num = [15, 50, 11, 88]
 # Add the two arrays together and store them in a new variable. Print the new array.
 blackhawk_numbers = blackhawks + player_num
 p blackhawk_numbers
+
+#At the bottom of the file, call the method to show that it works.
+p hockey("Crawford", "Kane", "Keith")
+
+#returns the array with the item added. 
+#Print a few test calls of the method.
+p madhouse([], "Chelsea Dagger")
+p madhouse([], blackhawks)
+p madhouse([], 88)
+
