09e09887c0f3ec8868005c4fa8b8850e475c0f8f
erika.schuebel <erika.schuebel@gmail.com>
2016-04-29 10:35:31 -0500

    Begin Nighthawk release

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,40 +1,80 @@
+#<--------------Assignment done as Nighthawk --------------->
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	words + "!" + " :)"
+  end
+end
+
+
+
+p Shout.yell_angrily("Hey")
+p Shout.yell_happily("Hey")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#<--------------Assignment done as Pocket Gopher --------------->
+# # module Shout
+
+# # 	def self.yell_angrily(words)
+# # 		p words + "!!!" + " :( "
+# # 	end
+
+# # 	def self.yelling_happliy(words)
+# # 		p words + "!" + " :-) "
+# # 	end
+
+# # end
+
+# # Shout.yell_angrily("Boooo")
+# # Shout.yelling_happliy("I am learning about modules")
+
 # module Shout
-
-# 	def self.yell_angrily(words)
-# 		p words + "!!!" + " :( "
+# 	def yell_angrily(words)
+# 		p words + "!!!" + " :-( "
 # 	end
 
-# 	def self.yelling_happliy(words)
-# 		p words + "!" + " :-) "
+# 	def make_me_wanna(words)
+# 		puts "You know you make me wanna #{words}!"
 # 	end
-
 # end
 
-# Shout.yell_angrily("Boooo")
-# Shout.yelling_happliy("I am learning about modules")
+# class TheIsleyBrothers
+# 	include Shout
+# end
 
-module Shout
-	def yell_angrily(words)
-		p words + "!!!" + " :-( "
-	end
+# class SmallChild
+# 	include Shout
+# end
 
-	def make_me_wanna(words)
-		puts "You know you make me wanna #{words}!"
-	end
-end
+# theIsleyBrothers = TheIsleyBrothers.new
+# theIsleyBrothers.make_me_wanna("SHOUT")
+# theIsleyBrothers.yell_angrily("The song is over")
 
-class TheIsleyBrothers
-	include Shout
-end
-
-class SmallChild
-	include Shout
-end
-
-theIsleyBrothers = TheIsleyBrothers.new
-theIsleyBrothers.make_me_wanna("SHOUT")
-theIsleyBrothers.yell_angrily("The song is over")
-
-smallChild = SmallChild.new
-smallChild.yell_angrily("Booooo")
-smallChild.make_me_wanna("SHOUT! Now wai-a-ait a minute")
+# smallChild = SmallChild.new
+# smallChild.yell_angrily("Booooo")
+# smallChild.make_me_wanna("SHOUT! Now wai-a-ait a minute")
