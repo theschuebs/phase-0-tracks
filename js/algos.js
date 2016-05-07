83bc40c945832592448474277c54eb475e5443ba
erika.schuebel <erika.schuebel@gmail.com>
2016-05-07 10:29:09 -0500

    Prepare release 2 and 3

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,36 @@
+// <------RELEASE 1 FIND A KEY-VALUE MATCH --------->
 
+// write a function that takes two objects and checks to see 
+// if the objects share at least one key-value pair. 
+// 	(You'll keep adding to this file, so we suggest grouping functions 
+// 		at the top and testing at the bottom of the file.) 
+// If we called your function with {name: "Steven", age: 54} 
+// and {name: "Tamir", age: 54}, the function would return true, 
+// because at least one key-value pair matches between the two objects. 
+// If no pairs match (and keep in mind that the two objects may not even have any of the same keys),
+//  the function should return false. To make your life easier, 
+//  don't worry about whether a property is a string ('age') or an identifier name (age). 
+//  Those can be considered equivalent. 
+//Add some driver code that tests both outcomes of your function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <------RELEASE 0 FIND THE LONGEST WORD --------->
 // Write a function 
 // This function will take an array of words or phrases
 // return the longest word in the array
@@ -11,7 +43,7 @@
 // if it's not longer, then the original phrase is the longest and the next phrase is compared to that.
 // Once all words have been compared, return the longest phrase. 
 
-var phrases = ["hi there", "cool", "how long will this take me"];
+var phrases = ["hi there", "cool", "how long will this take me", "a"];
 var characters = 0;
 var longestPhrase;
 
